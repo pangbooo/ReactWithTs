07789a439190d203e2ef7052be5ad9deb201fef3
pangbo <1838529223@qq.com>
2021-12-25 15:49:16 +0800

    component ProgressBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styles from './App.module.scss';
 import Button from './Components/Button';
+import ProgressBar from './Components/ProgressBar';
 
 function App() {
   function handleClick() {
@@ -17,6 +18,20 @@ function App() {
         <Button className={styles.mr10} type='pure'>pure</Button>
         <Button className={styles.mr10} type='primary' shape='circle'>circle</Button>
         <hr />
+      </div>
+
+      <div className={styles.container}>
+        <h2>ProgressBar</h2>
+        <ProgressBar
+          percent={20} 
+          themeColor={'red'}
+        />
+
+       <ProgressBar
+        percent={30}
+        themeColor="#6699FF"
+        statusScope={[[18, 'red'], [40, 'orange']]}
+      />
       </div>
       
     </div>
