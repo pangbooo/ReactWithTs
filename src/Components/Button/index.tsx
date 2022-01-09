03675e24d01e688144212a9e5c97227134753c55
pangbo <1838529223@qq.com>
2022-01-09 21:11:28 +0800

    fix lint

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -3,21 +3,21 @@ import classnames from 'classnames';
 import styles from './index.module.scss';
 
 type Props = {
-    children: React.ReactNode,
-    onClick?: ()  => void; // 对外暴露的点击事件
-    className?: string, // 自定义类名
-    type?: 'primary' | 'warning' | 'info' | 'default' | 'pure', // 按钮类型
-    shape?: 'circle' | 'radius', // 按钮形状 radius(默认)
-    block?: boolean, // 按钮展示 true | false(默认)
-}
+  children: React.ReactNode,
+  onClick?: ()  => void; // 对外暴露的点击事件
+  className?: string, // 自定义类名
+  type?: 'primary' | 'warning' | 'info' | 'default' | 'pure', // 按钮类型
+  shape?: 'circle' | 'radius', // 按钮形状 radius(默认)
+  block?: boolean, // 按钮展示 true | false(默认)
+};
 
 export default function Button(props: Props) {
-    const { children, onClick, className, type = '', shape = 'radius', block } = props;
-    return (
+  const { children, onClick, className, type = '', shape = 'radius', block } = props;
+  return (
         <div className={classnames(styles.xButton, styles.ripple, styles[type], styles[shape], block ?  styles.block : '', className)}
              onClick={onClick}
         >
             {children}
         </div>
-    )
+  );
 }
--- src/Components/ProgressBar/index.tsx
+++ src/Components/ProgressBar/index.tsx
@@ -1,60 +1,60 @@
 import styles from './index.module.scss';
 
 // [ [10, red], [20, white], [70, blue]]
-let sortArr = (arr: Array<[number, string]>) => arr.sort((a, b) => a[0] - b[0]);
+const sortArr = (arr: Array<[number, string]>) => arr.sort((a, b) => a[0] - b[0]);
 
 function checkStatus(
-    scope: Array<[number, string]>, 
-    val: number,
-    defaultColor: string
+  scope: Array<[number, string]>,
+  val: number,
+  defaultColor: string,
 ) {
-    val = +val;
-    sortArr(scope);
+  val = +val;
+  sortArr(scope);
 
-    if(scope.length === 1) {
-        return val < scope[0][0] ? scope[0][1] : defaultColor
-      }else if(scope.length === 2) {
-        return val < scope[0][0] ? scope[0][1]
-          : scope[0][0] < val && val < scope[1][0] ? scope[1][1]
-            : defaultColor
-      }else if(scope.length === 3) {
-        return val < scope[0][0] ? scope[0][1]
-          : scope[0][0] < val && val < scope[1][0] ? scope[1][1]
-            : scope[1][0] < val && val < scope[2][0] ? scope[2][1]
-              : defaultColor
-      }
+  if (scope.length === 1) {
+    return val < scope[0][0] ? scope[0][1] : defaultColor;
+  } else if (scope.length === 2) {
+    return val < scope[0][0] ? scope[0][1]
+      : scope[0][0] < val && val < scope[1][0] ? scope[1][1]
+        : defaultColor;
+  } else if (scope.length === 3) {
+    return val < scope[0][0] ? scope[0][1]
+      : scope[0][0] < val && val < scope[1][0] ? scope[1][1]
+        : scope[1][0] < val && val < scope[2][0] ? scope[2][1]
+          : defaultColor;
+  }
 }
 type Props = {
-    themeColor?: string,
-    percent?: number,
-    autoHidden?: boolean,
-    hiddenText?: boolean,
-    width?: number | string,
-    textColor?: string,
-    statusScope?: Array<[number, string]>, // 状态阈值,分别设置不同进度范围的进度条颜色,最大允许设置3个值, 为一个二维数组
-}
+  themeColor?: string,
+  percent?: number,
+  autoHidden?: boolean,
+  hiddenText?: boolean,
+  width?: number | string,
+  textColor?: string,
+  statusScope?: Array<[number, string]>, // 状态阈值,分别设置不同进度范围的进度条颜色,最大允许设置3个值, 为一个二维数组
+};
 
 export default function ProgressBar(props: Props) {
-    let { 
-        themeColor = '#06f', 
-        percent = 0, 
-        autoHidden = false, 
-        hiddenText = false, 
-        width = 320, 
-        textColor = '#666',
-        statusScope 
-      } = props
+  const {
+    themeColor = '#06f',
+    percent = 0,
+    autoHidden = false,
+    hiddenText = false,
+    width = 320,
+    textColor = '#666',
+    statusScope,
+  } = props;
 
-    return +percent === 100 && autoHidden
-        ? null
-        : (
+  return +percent === 100 && autoHidden
+    ? null
+    : (
             <div className={styles.progressWrap}>
                 <div className={styles.progressBar} style={{ width: typeof width === 'number' ? width + 'px' : width }}>
-                    <div className={styles.progressInnerBar} 
-                         style={{ 
-                             width: `${percent}%`,
-                             backgroundColor: statusScope && statusScope.length ? checkStatus(statusScope, percent, themeColor) : themeColor
-                            }}
+                    <div className={styles.progressInnerBar}
+                         style={{
+                           width: `${percent}%`,
+                           backgroundColor: statusScope && statusScope.length ? checkStatus(statusScope, percent, themeColor) : themeColor,
+                         }}
                     >
                     </div>
                 </div>
@@ -62,5 +62,5 @@ export default function ProgressBar(props: Props) {
                     !hiddenText && <div className={styles.progressText} style={{ color: textColor }}>{percent + '%'}</div>
                 }
             </div>
-        )
+    );
 }
